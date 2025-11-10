5096e28c794b5fe3fe12e0d899b15f610f5fd8ac
Miclasher <14122006mg@gmail.com>
2025-11-10 21:56:48 +0200

    Add project files.

---
+++ XMLReader-Advanced/Program.cs
@@ -0,0 +1,15 @@
+namespace XMLReader_Advanced
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainPage());
+        }
+    }
+}
